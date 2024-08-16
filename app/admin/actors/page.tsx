0578b108158b6a965cb82a26ad51916b273a19f4
Santiago Silverio <silveriosantiago373@gmail.com>
2024-08-16 19:56:54 -0300

    deploy 23

--- app/admin/actors/page.tsx
+++ app/admin/actors/page.tsx
@@ -72,12 +72,12 @@ const ActorsPage: React.FC = () => {
             setSortOrder(order);
             setSortColumn(column);
             const sortedActors = [...actors].sort((a, b) => {
-                  if (a[column] < b[column]) return order === 'asc' ? -1 : 1;
-                  if (a[column] > b[column]) return order === 'asc' ? 1 : -1;
-                  return 0;
+              if (a[column as keyof Actor] < b[column as keyof Actor]) return order === 'asc' ? -1 : 1;
+              if (a[column as keyof Actor] > b[column as keyof Actor]) return order === 'asc' ? 1 : -1;
+              return 0;
             });
             setActors(sortedActors);
-      };
+          };
 
       const handlePageChange = (page: number) => {
             setCurrentPage(page);
