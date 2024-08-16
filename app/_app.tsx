e06b8afd510aa55728d783da35308c0a090ad187
Santiago Silverio <silveriosantiago373@gmail.com>
2024-08-16 18:00:01 -0300

    deploy 11

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,11 +1,10 @@
-// En _app.tsx
+// _app.tsx
 import React from 'react';
-import { useClient } from 'next/client';
 import type { AppProps } from 'next/app';
-import '../globals.css'; // Asegúrate de importar tus estilos globales aquí si los tienes
+import '../globals.css'; // Import your global styles
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return useClient(
+  return (
     <div className="app">
       <Component {...pageProps} />
     </div>
