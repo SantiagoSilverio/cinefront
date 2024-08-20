1cc7691325ece5074c8c43ebe3c4629e93d3079c
Santiago Silverio <silveriosantiago373@gmail.com>
2024-08-19 23:47:26 -0300

    deploy 34

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import { useEffect, useState } from 'react';
 import Footer from './components/footer/Footer';
@@ -7,9 +7,17 @@ import MovieCarousel from './components/carousel/Carousel';
 import UpcomingReleases from './components/upcomingreleases/UpcomingReleases';
 import { getCurrentMovies, getUpcomingMovies } from './api/movies';
 
+type Movie = {
+  id: number;
+  title: string;
+  backdrop_path: string;
+  poster_path: string;
+  // Otros campos que puedas necesitar
+};
+
 export default function Home() {
-  const [currentMovies, setCurrentMovies] = useState([]);
-  const [upcomingMovies, setUpcomingMovies] = useState([]);
+  const [currentMovies, setCurrentMovies] = useState<Movie[]>([]);
+  const [upcomingMovies, setUpcomingMovies] = useState<Movie[]>([]);
 
   useEffect(() => {
     async function fetchMovies() {
@@ -24,23 +32,21 @@ export default function Home() {
 
   return (
     <div className="flex flex-col min-h-screen">
-
-      <main className="flex-grow flex flex-col items-center justify-between ">
-        <MovieCarousel posters={currentMovies.map(movie => ({
+      <main className="flex-grow flex flex-col items-center justify-between">
+        <MovieCarousel posters={currentMovies.map((movie: Movie) => ({
           src: `https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`,
           alt: movie.title,
           id: movie.id  // Incluye el ID de la película aquí
         }))} />
         <div className="mt-8">
           <h2 className="text-2xl font-bold mb-4">Próximos Estrenos</h2>
-          <UpcomingReleases posters={upcomingMovies.map(movie => ({
+          <UpcomingReleases posters={upcomingMovies.map((movie: Movie) => ({
             src: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
             alt: movie.title,
             id: movie.id  // Incluye el ID de la película aquí también si es necesario
           }))} />
         </div>
       </main>
-
-      </div>
+    </div>
   );
 }
