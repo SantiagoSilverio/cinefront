b1a913b4991d91e8f9c2c5506913cc484d9f1881
Santiago Silverio <silveriosantiago373@gmail.com>
2024-10-28 18:43:21 -0300

    deploy 124

--- middleware.js
+++ middleware.js
@@ -1,16 +1,54 @@
 import { NextResponse } from 'next/server';
 
-export function middleware(request) {
+export async function middleware(request) {
   const token = request.cookies.get('access_token');
-  
-  if (!token && !request.nextUrl.pathname.startsWith('/login') && !request.nextUrl.pathname.startsWith('/register')) {
+  const pathname = request.nextUrl.pathname;
+
+  // Permitir acceso público a "/"
+  if (pathname === '/') {
+    return NextResponse.next();
+  }
+
+  // Si no tiene token y no está en login o register, redirige a login
+  if (!token && !pathname.startsWith('/login') && !pathname.startsWith('/register')) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
-  
-  if (token && (request.nextUrl.pathname.startsWith('/login') || request.nextUrl.pathname.startsWith('/register'))) {
+
+  // Si tiene token y está en login o register, redirige a /
+  if (token && (pathname.startsWith('/login') || pathname.startsWith('/register'))) {
     return NextResponse.redirect(new URL('/', request.url));
   }
-  
+
+  // Verifica permisos si tiene token
+  if (token) {
+    const url = new URL('https://back-k1a3.onrender.com/user/', request.url);
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      const groups = data.groups;
+
+      if (groups.includes(1)) { 
+        // Si es administrador, redirige a /admin
+        if (pathname !== '/admin') {
+          return NextResponse.redirect(new URL('/admin', request.url));
+        }
+      } else {
+        // Si no es administrador, redirige a /
+        if (pathname === '/admin') {
+          return NextResponse.redirect(new URL('/', request.url));
+        }
+      }
+    } else {
+      // Si no se pudo verificar los permisos, redirige a /
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  }
+
   return NextResponse.next();
 }
 
