172ee3a97dbe1a2d96b6b343b8bbe2bc1733c0d0
Santiago Silverio <silveriosantiago373@gmail.com>
2024-10-28 19:37:11 -0300

    deploy 135

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import MovieCarousel from './components/carousel/Carousel';
 import UpcomingReleases from './components/upcomingreleases/UpcomingReleases';
 import { getCurrentMovies, getUpcomingMovies } from './api/movies';
+import "./globals.css";
 
 interface Movie {
   id: number;
@@ -17,80 +18,45 @@ interface Movie {
 export default function Home() {
   const [currentMovies, setCurrentMovies] = useState<Movie[]>([]);
   const [upcomingMovies, setUpcomingMovies] = useState<Movie[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     async function fetchMovies() {
-      try {
-        setIsLoading(true);
-        const [currentMoviesData, upcomingMoviesData] = await Promise.all([
-          getCurrentMovies(),
-          getUpcomingMovies()
-        ]);
-        
-        setCurrentMovies(currentMoviesData);
-        setUpcomingMovies(upcomingMoviesData);
-      } catch (err) {
-        console.error('Error fetching movies:', err);
-        setError('Failed to load movies. Please try again later.');
-      } finally {
-        setIsLoading(false);
-      }
+      const currentMoviesData: Movie[] = await getCurrentMovies();
+      const upcomingMoviesData: Movie[] = await getUpcomingMovies();
+      console.log("Current Movies:", currentMoviesData); // Verifica los datos aquí
+      setCurrentMovies(currentMoviesData);
+      setUpcomingMovies(upcomingMoviesData);
     }
   
     fetchMovies();
   }, []);
 
-  if (error) {
-    return (
-      <div className="flex justify-center items-center min-h-screen">
-        <div className="text-red-500">{error}</div>
-      </div>
-    );
-  }
-
-  if (isLoading) {
-    return (
-      <div className="flex justify-center items-center min-h-screen">
-        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-pink-500"></div>
-      </div>
-    );
-  }
-
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen  overflow-y-scroll">
       <main className="flex-grow flex flex-col items-center justify-between">
-        {currentMovies.length > 0 && (
-          <MovieCarousel
-            logos={currentMovies.map(movie => ({
-              src: `https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`,
+        <MovieCarousel
+          logos={currentMovies.map(movie => ({
+            src: `https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`,
+            alt: movie.title,
+            id: movie.id
+          }))}   
+          posters={currentMovies.map(movie => ({
+            src: `https://image.tmdb.org/t/p/w1280${movie.poster_path}`,
+            alt: movie.title,
+            overview: movie.overview,
+            id: movie.id
+          }))}   
+        />
+        <div className="mt-4">
+          <h2 className="text-2xl font-bold mb-4 p-5 rounded-lg bg-[#D5447B]">Próximos Estrenos</h2>
+          <UpcomingReleases
+            posters={upcomingMovies.map(movie => ({
+              src: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
               alt: movie.title,
               id: movie.id
-            }))}   
-            posters={currentMovies.map(movie => ({
-              src: `https://image.tmdb.org/t/p/w1280${movie.poster_path}`,
-              alt: movie.title,
-              overview: movie.overview,
-              id: movie.id
-            }))}   
+            }))}
           />
-        )}
-        
-        {upcomingMovies.length > 0 && (
-          <div className="mt-4 w-full px-4">
-            <h2 className="text-2xl font-bold mb-4 p-5 rounded-lg bg-[#D5447B] text-center">
-              Próximos Estrenos
-            </h2>
-            <UpcomingReleases
-              posters={upcomingMovies.map(movie => ({
-                src: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-                alt: movie.title,
-                id: movie.id
-              }))}
-            />
-          </div>
-        )}
+        </div>
       </main>
     </div>
   );
--- app/template.tsx
+++ app/template.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"; // Este archivo es un componente de cliente
 
 import { usePathname } from "next/navigation";
 import Navbar from "./components/navbar/Navbar";
@@ -6,14 +6,13 @@ import Footer from "./components/footer/Footer";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const isLoginPage = pathname === "/login";
+  const isLoginPage = pathname === "/login"; // Verificamos si estamos en login
 
   return (
     <>
+    {}
       {!isLoginPage && <Navbar />}
-      <div className="min-h-screen">
-        {children}
-      </div>
+      {children}
       {!isLoginPage && <Footer />}
     </>
   );
