f1427982920d0d1cc9f23c1a4f3da4de0a0e37ca
Santiago Silverio <silveriosantiago373@gmail.com>
2024-10-28 19:39:16 -0300

    deploy 136

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">          
       <body className={inter.className}>
         <AuthProvider>
-          <Template>{children}</Template>
+          {children}
         </AuthProvider>
       </body>
     </html>
