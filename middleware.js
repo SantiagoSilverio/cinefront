82b37faacf9c1bdc2c816e2bd3d99d4de8e81ef3
SantiagoSilverio <130712793+SantiagoSilverio@users.noreply.github.com>
2024-12-02 17:49:27 -0300

    deploy 158

--- middleware.js
+++ middleware.js
@@ -1,61 +1,16 @@
-
 import { NextResponse } from 'next/server';
 
-
-export async function middleware(request) {
+export function middleware(request) {
   const token = request.cookies.get('access_token');
-  const pathname = request.nextUrl.pathname;
-
-  // Permitir acceso público a "/"
-  if (pathname === '/') {
-    return NextResponse.next();
-  }
-
-  // Si no tiene token y no está en login o register, redirige a login
-  if (!token && !pathname.startsWith('/login') && !pathname.startsWith('/register')) {
+  
+  if (!token && !request.nextUrl.pathname.startsWith('/login') && !request.nextUrl.pathname.startsWith('/register')) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
-
-  // Si tiene token y está en login o register, redirige a /
-  if (token && (pathname.startsWith('/login') || pathname.startsWith('/register'))) {
+  
+  if (token && (request.nextUrl.pathname.startsWith('/login') || request.nextUrl.pathname.startsWith('/register'))) {
     return NextResponse.redirect(new URL('/', request.url));
   }
-
-  // Verifica permisos si tiene token
-  if (token) {
-    const url = new URL('https://back-k1a3.onrender.com/user', request.url);
-    const response = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      }
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      const groups = data.groups; 
-
-      // Si es administrador y no está en /admin, redirige a /admin
-      if (groups.includes('administrador')) {
-        console.log('es administrador');
-        if (pathname !== '/admin') {
-          return NextResponse.redirect(new URL('/admin', request.url));
-        } else {
-          return NextResponse.next();
-        }
-      } else {
-        console.log('No es administrador');
-        if (pathname === '/admin') {
-          return NextResponse.redirect(new URL('/', request.url));
-        } else {
-          return NextResponse.next();
-        }
-      }
-    } else {
-      // Si no se pudo verificar los permisos, redirige a /
-      return NextResponse.redirect(new URL('/', request.url));
-    }
-  }
-
+  
   return NextResponse.next();
 }
 
